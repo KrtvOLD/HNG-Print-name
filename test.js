ef6c1786afec27d17e31cb0bf782de4e7a21335a
KrtvOLD <72816696+KrtvOLD@users.noreply.github.com>
2021-08-18 18:53:52 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+const person = {
+    id: "realLehre",
+    name: "David Omolere Egbuwalo"
+};
+
+// print name in console
+if(person.id === "realLehre"){
+    console.log(person.name);
+    alert(person.name)
+} else {
+    console.log("this user is not found");
+}
